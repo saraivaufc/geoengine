16450fc6a3d27d55c1ae68aef6416d2de00ce80e
Ciano Saraiva <saraiva.ufc@gmail.com>
2019-11-07 01:25:14 -0300

    atualiza as configurações

--- ge/db/__init__.py
+++ ge/db/__init__.py
@@ -2,8 +2,8 @@ import os
 
 from mongoengine import connect
 
-DB_NAME = os.environ.get("GEOENGINE_DB_NAME", "earth_engine")
-DB_HOST = os.environ.get("GEOENGINE_DB_HOST", "localhost")
+DB_NAME = os.environ.get("ENGINE_DB_NAME", "engine")
+DB_HOST = os.environ.get("ENGINE_DB_HOST", "localhost")
 
 connect(host='mongodb://{DB_HOST}/{DB_NAME}'.format(
     DB_HOST=DB_HOST,
